9db098cfe6565106aaa84a54950998ac0fb0fe81
Vic-L <vljc17@gmail.com>
2018-02-06 22:08:35 +0800

    feat: onRSSObjectLost

--- handler.js
+++ handler.js
@@ -19,11 +19,11 @@ module.exports.onUpload = (event, context, callback) => {
   
   // const srcS3 = JSON.parse(event.body).Records[0].s3 // for dev in sls offline
   const srcS3 = event.Records[0].s3 // in production with S3 event as input
-  const srcBucketName = srcS3.bucket.name
+  const srcBucket = srcS3.bucket.name
   const srcKey = srcS3.object.key
 
   const srcParams = {
-    Bucket: srcBucketName,
+    Bucket: srcBucket,
     Key: srcKey
   }
 
@@ -71,4 +71,69 @@ module.exports.onUpload = (event, context, callback) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.onRRSObjectLost = (event, context, callback) => {
+  
+  const srcBucket = process.env.SRC_BUCKET_NAME;
+
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+  })
+
+  //** event.body is used for debugging in sls offline 
+  //** event is a string when debugging using postman
+  //** event is a JSON when triggered by an S3 event
+  
+  // const record = JSON.parse(event.body).Records[0] // for dev in sls offline
+  const record = event.Records[0] // in production with S3 event as input
+  const extension = key.split(".").pop()
+
+  const width = parseInt(key.split("_").pop().split(".").shift())
+
+  const srcKey = key.replace(/\_[0-9]+\..*$/i, `.${extension}`)
+
+  const srcParams = {
+    Bucket: srcBucket,
+    Key: srcKey
+  }
+
+  s3.getObject(srcParams)
+  .promise()
+  .then(function(data) {
+    resize(data.Body, width, function(err, buffer) {
+      if (err) return callback(null, handleS3Error(err));
+
+      const bucketName = record.s3.bucket.name
+      const key = record.s3.object.key
+      const dstParams = {
+        Bucket: bucketName,
+        Key: key,
+        Body: buffer,
+        StorageClass: "REDUCED_REDUNDANCY",
+        ACL: "public-read",
+        ContentType: data.ContentType
+      }
+
+      s3.upload(dstParams)
+      .promise()
+      .then(function(data) {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({
+            message: data,
+          }),
+        };
+
+        callback(null, response);
+      })
+      .catch(function(err) {
+        callback(null, handleS3Error(err))
+      })
+    })
+  })
+  .catch(function(err) {
+    callback(null, handleS3Error(err))
+  })
 };
